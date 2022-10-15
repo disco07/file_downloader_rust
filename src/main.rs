ad17a05c86cd73fc70d5e15278d3e64b7284cf42
disco07 <koneenok@outlook.fr>
2022-10-15 03:27:30 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
+use reqwest::header::HeaderValue;
+
 struct FilePart {
     name: String,
     start: i64,
     end: i64,
 }
 
-async fn downloader(url: &str){
-    // assert_eq!(url.trim(), "", "invalid url");
+pub trait HeaderValueExtension {
+    fn to_string(&self) -> String;
+}
+
+impl HeaderValueExtension for HeaderValue {
+    fn to_string(&self) -> String {
+        self.to_str().unwrap_or_default().to_string()
+    }
+}
+
+async fn downloader(url: &str) -> Result<(), &str> {
+    if url == "" {
+        return Err("invalid url");
+    }
     let client = reqwest::Client::new();
     let res = client
         .head(url)
         .send()
         .await;
 
+    let mut accept_range: bool = false;
+    let content_length: i64;
     match res {
-        Ok(f) => println!("{:?}", f.headers().get("accept-ranges")),
+        Ok(f) => {
+            content_length = f.headers().get("content-length").unwrap().to_string().parse::<i64>().unwrap();
+            accept_range = f.headers().get("accept-ranges").unwrap().to_string() == "bytes";
+            println!("{:?} - {:?}", content_length, accept_range)
+        },
         Err(e) => println!("{}", e)
+    };
+
+    if !accept_range {
+        return Err("unable to download file with multithreads");
     }
 
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), &'static str> {
     downloader("https://agritrop.cirad.fr/584726/1/Rapport.pdf").await
 }
